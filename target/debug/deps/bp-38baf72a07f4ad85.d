8fa002bb8d8d3a993cb30d9e1c720b92a1e9273e
Sofia Langer-Osuna <sofialangerosuna@gmail.com>
2024-02-05 19:10:17 -0800

    removed target, I'm stupid

--- target/debug/deps/bp-38baf72a07f4ad85.d
+++
@@ -1,7 +0,0 @@
-/home/slangerosuna/proj/bplang/target/debug/deps/bp-38baf72a07f4ad85: src/main.rs src/lexer.rs src/syntax_tree_gen.rs
-
-/home/slangerosuna/proj/bplang/target/debug/deps/bp-38baf72a07f4ad85.d: src/main.rs src/lexer.rs src/syntax_tree_gen.rs
-
-src/main.rs:
-src/lexer.rs:
-src/syntax_tree_gen.rs:
--- target/debug/deps/bp-ba865e081cb57a97.d
+++
@@ -1,7 +0,0 @@
-/home/slangerosuna/proj/bplang/target/debug/deps/libbp-ba865e081cb57a97.rmeta: src/main.rs src/lexer.rs src/syntax_tree_gen.rs
-
-/home/slangerosuna/proj/bplang/target/debug/deps/bp-ba865e081cb57a97.d: src/main.rs src/lexer.rs src/syntax_tree_gen.rs
-
-src/main.rs:
-src/lexer.rs:
-src/syntax_tree_gen.rs:
